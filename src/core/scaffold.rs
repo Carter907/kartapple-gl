63d2d9d1d49db0fddee811f60b23d5b62fa341e0
carter <speers.carter@gmail.com>
2023-09-17 16:22:50 -0400

    removed on_key

--- src/core/scaffold.rs
+++ src/core/scaffold.rs
@@ -4,16 +4,6 @@ use glfw::{Action, Key, Modifiers, Scancode, WindowEvent};
 pub trait AppScaffold {
     unsafe fn on_init(&mut self, app: &mut KartApple);
     unsafe fn on_loop(&mut self, app: &mut KartApple);
-    unsafe fn on_key(
-        &mut self,
-        key: Key,
-        scan_code: Scancode,
-        action: Action,
-        modifiers: Modifiers,
-        app: &mut KartApple,
-    );
-
-    
     unsafe fn on_resize(&mut self, width: i32, height: i32, app: &mut KartApple);
     unsafe fn on_clean(&mut self, app: &mut KartApple);
 }
